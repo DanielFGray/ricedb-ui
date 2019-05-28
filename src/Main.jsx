de30d85d71280afad984487c4e41a83deb6ac7b7
DanielFGray <DanielFGray@gmail.com>
2019-05-28 06:16:42 -0500

    rewrite main class component into hooks

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,57 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Nav from './Nav'
 
 import UserList from './Users'
 import Display from './Display'
 
-export const Stringify = ({ className, style, ...data }) => (
-  <pre className={className} style={style}>
-    {JSON.stringify(data, null, 2)}
-  </pre>
-)
-
 const has = (k, o) => Object.prototype.hasOwnProperty.call(o, k)
 
-export default class Main extends React.Component {
-  state = {
-    user: null,
-    category: 'all',
+const Main = ({ data }) => {
+  const [user, userChange] = useState(null)
+  const [category, categoryChange] = useState('all')
+
+  const selectUser = user => () => {
+    userChange(user)
   }
 
-  selectUser = user => () => {
-    this.setState({ user })
+  const selectCategory = category => () => {
+    categoryChange(category)
   }
 
-  selectCategory = category => () => {
-    this.setState({ category })
-  }
-
-  render() {
-    const { data } = this.props
-    const { category } = this.state
-    const userList =
-      this.state.category !== 'all'
-        ? Object.entries(data)
-            .filter(([, value]) => has(category, value))
-            .reduce((p, [k, v]) => Object.assign(p, { [k]: v }), {})
-        : data
-    return (
-      <div className="wrapper">
-        <Nav
-          data={data}
-          activeCategory={category}
-          selectCategory={this.selectCategory}
-        />
-        <div class="displayarea">
-          {this.state.user &&
-            Display({
-              category,
-              name: this.state.user,
-              ...data[this.state.user],
-            })}
-        </div>
-        <UserList data={userList} selectUser={this.selectUser} />
+  const userList = category !== 'all'
+    ? Object.entries(data)
+        .filter(([, value]) => has(category, value))
+        .reduce((p, [k, v]) => Object.assign(p, { [k]: v }), {})
+    : data
+  return (
+    <div className="wrapper">
+      <Nav
+        data={data}
+        activeCategory={category}
+        selectCategory={selectCategory}
+      />
+      <div class="displayarea">
+        {user &&
+          Display({
+            category,
+            name: user,
+            ...data[user],
+          })}
       </div>
-    )
-  }
+      <UserList data={userList} selectUser={selectUser} />
+    </div>
+  )
 }
+
+export default Main
