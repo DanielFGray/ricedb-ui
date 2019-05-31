74728fd096c6144208cfab3a30fa924414b62996
DanielFGray <DanielFGray@gmail.com>
2019-05-31 02:43:41 -0500

    better loading and error indicators

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import GetJson from './GetJson'
 import Main from './Main'
+import { Message, Icon } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 import './style.css'
 
@@ -11,7 +12,7 @@ const Index = () => {
   )
   return (
     <>
-      {error && error.message}
+      {error && <Message warning><Icon name="warning" />{error.message}</Message>}
       <Main
         data={data || {}}
         loading={loading}
